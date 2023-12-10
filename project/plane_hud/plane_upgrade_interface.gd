e0cf64a8ec1ab0823f28e8815d4fc704605f5596
the-alex-g <alex.gestwicki@gmail.com>
2023-12-10 15:22:48 -0500

    Fix multiple upgrade menus not working

--- project/plane_hud/plane_upgrade_interface.gd
+++ project/plane_hud/plane_upgrade_interface.gd
@@ -8,6 +8,8 @@ signal item_upgraded(item_name)
 @export var launch_button := JOY_BUTTON_START
 @export var upgrade_button := JOY_BUTTON_A
 @export var upgrades : Array[UpgradableItem] = []
+@export var selection_up := JOY_BUTTON_DPAD_UP
+@export var selection_down := JOY_BUTTON_DPAD_DOWN
 
 var _resources := 0
 
@@ -16,6 +18,7 @@ var _resources := 0
 
 
 func _ready()->void:
+	upgrades = _get_upgrades_duplicate()
 	hide()
 
 
@@ -30,6 +33,17 @@ func _input(event:InputEvent)->void:
 					launched.emit()
 				upgrade_button:
 					_upgrade_selected_item()
+				selection_down:
+					_change_selection(1)
+				selection_up:
+					_change_selection(-1)
+
+
+func _get_upgrades_duplicate()->Array[UpgradableItem]:
+	var upgrades_duplicate : Array[UpgradableItem] = []
+	for upgrade:UpgradableItem in upgrades:
+		upgrades_duplicate.append(upgrade.duplicate())
+	return upgrades_duplicate
 
 
 func _upgrade_selected_item()->void:
@@ -46,10 +60,19 @@ func _upgrade_selected_item()->void:
 			_resources_label.text = str(_resources)
 
 
+func _change_selection(direction:int)->void:
+	var selected_items := _item_list.get_selected_items()
+	if selected_items.size() > 0:
+		var selected_index := selected_items[0]
+		selected_index += sign(direction)
+		selected_index %= _item_list.item_count
+		_item_list.deselect_all()
+		_item_list.select(selected_index)
+
+
 func open(kills:int)->void:
 	show()
 	_resources += _kills_to_resources(kills)
-	_item_list.grab_focus()
 	_generate_item_list()
 	_resources_label.text = str(_resources)
 
@@ -60,14 +83,13 @@ func _kills_to_resources(kills:int)->int:
 	return (kills * kills + kills) / 2.0
 
 
-func _generate_item_list(selected_item_index := -1)->void:
+func _generate_item_list(selected_item_index := 0)->void:
 	_item_list.clear()
 	_create_item_list(selected_item_index)
 
 
-func _create_item_list(selected_item_index := -1)->void:
+func _create_item_list(selected_item_index := 0)->void:
 	for item:UpgradableItem in upgrades:
 		_item_list.add_item(item.name + ": " + str(item.cost))
 	
-	if selected_item_index != -1:
-		_item_list.select(selected_item_index)
+	_item_list.select(selected_item_index)
