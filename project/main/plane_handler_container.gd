707391d0293c13a253d44a4d1b84d52256167cc7
the-alex-g <alex.gestwicki@gmail.com>
2023-12-17 12:08:49 -0500

    Add support for multiple AI planes

--- project/main/plane_handler_container.gd
+++ project/main/plane_handler_container.gd
@@ -1,6 +1,8 @@
 class_name PlaneHandlerContainer
 extends GridContainer
 
+@export var ai_planes := 2
+
 var _plane_handlers := {}
 
 
@@ -10,7 +12,8 @@ func generate_plane_handlers(player_colors:Dictionary)->void:
 	else:
 		columns = 2
 	
-	_add_AI_plane_handler()
+	for i:int in ai_planes:
+		_add_AI_plane_handler(-i - 1)
 	
 	for player_index:int in player_colors.keys():
 		_add_plane_handler(player_index, player_colors[player_index])
@@ -18,10 +21,10 @@ func generate_plane_handlers(player_colors:Dictionary)->void:
 	_sync_radar()
 
 
-func _add_AI_plane_handler()->void:
+func _add_AI_plane_handler(index:int)->void:
 	var plane_handler := AIPlaneHandler.new()
-	_plane_handlers[-1] = plane_handler
-	plane_handler.plane_destroyed.connect(_on_plane_handler_plane_destroyed.bind(-1))
+	_plane_handlers[index] = plane_handler
+	plane_handler.plane_destroyed.connect(_on_plane_handler_plane_destroyed.bind(index))
 	add_child(plane_handler)
 
 
@@ -37,7 +40,7 @@ func _add_plane_handler(player_index:int, color:Color)->void:
 
 
 func _on_plane_handler_plane_destroyed(destroyer_index:int, destroyed_plane_index:int)->void:
-	if destroyer_index != -1:
+	if destroyer_index >= 0:
 		_plane_handlers[destroyer_index].log_kill(_plane_handlers[destroyed_plane_index].color)
 
 
